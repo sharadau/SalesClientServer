9f4d7648e9beb7d73f4b27dab0ed529f04a1e795
abhishek789 <abhishek.somani@synerzip.com>
2015-04-16 13:08:58 +0530

    modified base url

--- public/config/config.js
+++ public/config/config.js
@@ -1,6 +1,6 @@
 /**
  * Created by sharadau on 07-04-2015.
  */
-var service_base_url = 'https://boiling-eyrie-9085.herokuapp.com';
-//var service_base_url = 'https://enigmatic-tor-9292.herokuapp.com';
+//var service_base_url = 'https://boiling-eyrie-9085.herokuapp.com';
+var service_base_url = 'https://desolate-crag-3719.herokuapp.com';
 //var service_base_url = 'http://localhost:3000';
