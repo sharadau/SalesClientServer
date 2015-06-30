470dfe622e048174735b010dacc4141ff50ff96a
abhishek789 <abhishek.somani@synerzip.com>
2015-06-30 17:55:31 +0530

    modify email popup

---
+++ controllers/fileupload.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sharadau on 2/5/2015.
+ */
+//var mongoose = require('mongoose'),
+  //  BillableStatus = mongoose.model('billable_status');
+
+exports.list=function(req,res,next){
+    res.send("list success upload");
+};
+exports.create=function(req,res){
+    res.send("File uploaded successfully");
+
+};
+
+
+
+
